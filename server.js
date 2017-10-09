b46bd83b440f93612fe7d2bd017515aa6eb97922
James Jarvis <jamesjarvis@Jamess-iMac.local>
2017-10-08 20:33:01 -0400

    server

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+app.get('*', (req, res, next) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
