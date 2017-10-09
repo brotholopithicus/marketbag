ec5247d67f8c3049d8be60a830367f9bfd992c8c
James Jarvis <jamesjarvis@Jamess-iMac.local>
2017-10-08 20:29:28 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+<div id="app">
+  <div class="container">
+    <hello-world/>
+  </div>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+:root {
+  /*--bg-overlay: rgba(184, 163, 121, 0.35);*/
+  --bg-overlay: rgba(184, 163, 121, 0.3);
+}
+
+html {
+  background: linear-gradient( var(--bg-overlay),
+  var(--bg-overlay)), url(../assets/image0006_270.jpg) no-repeat center center fixed;
+  background-size: cover;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>Anya &amp; Greer Market Bag</h1>
+    <h2>Slowing Down Fast Fashion</h2>
+    <!-- <img src="../assets/image0007.jpg" /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  max-width: 50%;
+  height: auto;
+  transform: rotate(270deg);
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
